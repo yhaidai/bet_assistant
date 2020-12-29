f8e1f51cc6b40c8c4dcc58bbbd2144b81de5b70b
Yaroslav Haidai <yhaidai@ukr.net>
2020-12-29 21:17:55 +0200

    Added constants

---
+++ src/constants.py
@@ -0,0 +1,8 @@
+SPORT_FULL_NAMES_TO_SHORT = {
+        'Counter-Strike: Global Offensive': 'csgo',
+        'Dota 2': 'dota',
+        'League of Legends': 'lol',
+        'Football': 'football',
+    }
+SPORT_SHORT_NAMES_TO_FULL = {v: k for k, v in SPORT_FULL_NAMES_TO_SHORT.items()}
+SPORT_NAME = 'csgo'
